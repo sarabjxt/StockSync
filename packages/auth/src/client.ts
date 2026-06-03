ff0512ac9188bf5b1f589edde4499e3d54adb97b
Sarabjit Singh <heysarabjit@gmail.com>
2026-06-04 01:08:58 +0530

    fix: better auth client env baseUrl value

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,12 +1,20 @@
 import { emailOTPClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 
-export const authClient = createAuthClient({
-  plugins: [emailOTPClient()],
-  baseURL:
+function getBaseUrl() {
+  const envUrl =
     (typeof process !== "undefined"
       ? process.env.BETTER_AUTH_URL
-      : undefined) || "http://localhost:8000",
+      : undefined) ||
+    // @ts-ignore - this will run fine in vite
+    (import.meta as any).env?.BETTER_AUTH_URL
+
+  return envUrl || "http://localhost:8000"
+}
+
+export const authClient = createAuthClient({
+  plugins: [emailOTPClient()],
+  baseURL: getBaseUrl(),
 })
 
 export type Session = typeof authClient.$Infer.Session
